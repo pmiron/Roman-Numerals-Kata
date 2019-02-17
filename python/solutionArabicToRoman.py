fad3a0ef92fd001cd9d7ec80ec4c4bebb7535198
Pavel Miron <pmiron@outlook.com>
2019-02-17 02:49:43 +0100

    Python TDD Refactor Solution for Number 4

--- python/solutionArabicToRoman.py
+++ python/solutionArabicToRoman.py
@@ -1,9 +1,10 @@
 def arabic_to_roman(number):
-    if number == 4:
-        return 'IV'
     result_roman_number = ''
-    for i in range(number):
-        result_roman_number += 'I'
+    if number == 4:
+        result_roman_number = 'IV'
+    else:
+        for i in range(number):
+            result_roman_number += 'I'
 
     return result_roman_number
 
