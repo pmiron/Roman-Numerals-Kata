ccb979cc443e8dc9926624e9a6884f725296e284
Pavel Miron <pmiron@outlook.com>
2019-02-17 18:43:50 +0100

    Python TDD Refactor for common values

--- python/solutionArabicToRoman.py
+++ python/solutionArabicToRoman.py
@@ -1,17 +1,22 @@
 def arabic_to_roman(number):
     remaining = number
     result_roman_number = ''
-    if remaining == 9:
-        result_roman_number = 'IX'
-        remaining -= 9
-    elif remaining >= 5:
-        result_roman_number = 'V'
-        remaining -= 5
-    elif number == 4:
-        result_roman_number = 'IV'
-        remaining -= 4
+
+    remaining, result_roman_number = append_roman_numerals(remaining, 9, 'IX', result_roman_number)
+    remaining, result_roman_number = append_roman_numerals(remaining, 5, 'V', result_roman_number)
+    remaining, result_roman_number = append_roman_numerals(remaining, 4, 'IV', result_roman_number)
+
     for i in range(remaining):
         result_roman_number += 'I'
 
     return result_roman_number
+
+
+def append_roman_numerals(arabic, arabicKeyValue, romanKeyValue, romanDigitsBuilder):
+    result = arabic
+    if result >= arabicKeyValue:
+        romanDigitsBuilder += romanKeyValue
+        result -= arabicKeyValue
+    return result, romanDigitsBuilder
+
 
